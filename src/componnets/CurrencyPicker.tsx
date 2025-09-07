e5598ad278a1635350ec9c614c09d15295163e5e
Konstantin Karasevich <kkv_np@mail.ru>
2025-09-07 14:04:48 +0300

    added keyboard support

--- src/componnets/CurrencyPicker.tsx
+++ src/componnets/CurrencyPicker.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react"
+import { KeyboardEvent, useCallback, useEffect, useRef, useState } from "react"
 import Label from "./Label"
 import CloseSVG from "../svg/CloseSVG"
 import CurrencyList from "../resources/currencies.json"
@@ -17,12 +17,28 @@ const CurrencyPicker = ({ value, onChanged }: IProps) => {
     const [showModal, setShowModal] = useState(false)
     const [filterText, setFilterText] = useState('')
     const [filteredData, setFilteredData] = useState<Currency[]>([])
+    const [focusedIndex, setFocusedIndex] = useState(-1)
 
     const handleItemSelected = useCallback((currency: Currency) => {
         onChanged(currency)
         setShowModal(false)
         setFilterText('')
     }, [onChanged])
+
+    const handleKeyDown = useCallback((e: KeyboardEvent<HTMLDialogElement>) => {
+        console.log('keyDown', e.key)
+        switch (e.key) {
+            case "ArrowDown":
+                setFocusedIndex(v => Math.min(v + 1, filteredData.length - 1))
+                break
+            case "ArrowUp":
+                setFocusedIndex(v => Math.max(v - 1, 0))
+                break
+            case "Enter":
+                onChanged(filteredData[focusedIndex])
+                break
+        }
+    }, [filteredData, focusedIndex, onChanged])
 
     useEffect(() => {
         if (!filterText) {
@@ -38,10 +54,23 @@ const CurrencyPicker = ({ value, onChanged }: IProps) => {
             //scroll to selected element
             const targetElement = document.getElementById(value.code)
             targetElement?.scrollIntoView()
+            
         } else {
             dialogRef.current?.close()
         }
     }, [showModal, value])
+
+    useEffect(() => {
+        const index = filteredData.findIndex(v => v.code === value.code)
+        setFocusedIndex(index)
+    }, [filteredData, value])
+
+    useEffect(() => {
+        if (focusedIndex !== -1) {
+            const targetElement = document.getElementById(filteredData[focusedIndex].code)
+            targetElement?.scrollIntoView({ behavior: 'smooth' })
+        }
+    }, [focusedIndex, filteredData])
 
     return (
         <div className="w-full">
@@ -55,7 +84,7 @@ const CurrencyPicker = ({ value, onChanged }: IProps) => {
                 </div>
             </button>
             {showModal && (
-                <dialog ref={dialogRef} className="rounded-sm p-[16px] w-full lg:w-[440px] h-[450px] lg:h-[439px] flex flex-col gap-[12px]" onClose={() => setShowModal(false)}>
+                <dialog ref={dialogRef} className="rounded-sm p-[16px] w-full lg:w-[440px] h-[450px] lg:h-[439px] flex flex-col gap-[12px]" onClose={() => setShowModal(false)} onKeyDown={handleKeyDown}>
                     <div className="flex justify-between">
                         <p className="text-[16px] font-semibold">Select currency</p>
                         <button onClick={() => setShowModal(false)}><CloseSVG /></button>
@@ -63,13 +92,14 @@ const CurrencyPicker = ({ value, onChanged }: IProps) => {
                     <Label>Choose a currency from the list below or use the search bar to find a specific currency.</Label>
                     <SearchInput onChanged={setFilterText} />
                     <div className="flex-1 overflow-auto">
-                        {filteredData.map(d => {
+                        {filteredData.map((d, i) => {
                             const selected = value.code === d.code
                             return (
                                 <button 
+                                    tabIndex={i}
                                     id={d.code}
                                     key={d.name} 
-                                    className={`flex items-center gap-[12px] px-[8px] py-[12px] w-full rounded-xs hover:bg-neutral-100 cursor-pointer ${selected ? 'bg-neutral-100' : ''}`}
+                                    className={`flex items-center gap-[12px] px-[8px] py-[12px] w-full rounded-xs hover:bg-neutral-100 cursor-pointer ${focusedIndex === i ? 'bg-neutral-100' : ''}`}
                                     onClick={() => handleItemSelected(d)}>
                                     <div className="flex items-center justify-center rounded-full border border-blue-200 bg-blue-50 w-[30px] h-[30px]">
                                         <p className="text-blue-700 font-semibold text-[12px] max-w-[30px] overflow-hidden">{d.symbolNative}</p>
--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -1,25 +1,36 @@
 import useExchangeState from "../states/exchangeState"
 import AmounForm from "./AmounForm"
+import AmounFormMobile from "./AmounFormMobile"
 import FormHeader from "./FormHeader"
 import ResultPanel from "./ResultPanel"
+import ResultPanelMobile from "./ResultPanelMobile"
 
 const MainContainer  = () => {
 
     const { isLoadingData } = useExchangeState()
 
     return (
-        <div className="relative w-full lg:w-[1000px] items-center">
-            <FormHeader />
-            <div className="w-full flex flex-col lg:flex-row gap-[30px] mt-[30px]">
-                <AmounForm />
-                <ResultPanel />
-            </div>
-            {isLoadingData && (
-                <div className="absolute flex items-center justify-center bg-white-tranparent top-0 left-0 right-0 bottom-0">
-                    <p className="text-[18px] font-semibold">Loading...</p>
+        <>
+            {/* Desktop area */}
+            <div className="relative w-full lg:w-[1000px] items-center hidden sm:block">
+                <FormHeader />
+                <div className="w-full flex flex-col lg:flex-row gap-[30px] mt-[30px]">
+                    <AmounForm />
+                    <ResultPanel />
                 </div>
-            )}
-        </div>
+                {isLoadingData && (
+                    <div className="absolute flex items-center justify-center bg-white-tranparent top-0 left-0 right-0 bottom-0">
+                        <p className="text-[18px] font-semibold">Loading...</p>
+                    </div>
+                )}
+            </div>
+
+            {/* Mobile area */}
+            <div className="relative w-full h-full sm:hidden flex flex-col gap-[24px] items-center justify-center">
+                <AmounFormMobile />
+                <ResultPanelMobile />
+            </div>
+        </>
     )
 }
 
